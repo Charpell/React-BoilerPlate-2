673cfeef32d0c84a06f30cdbc6375da80fcd55e9
Charpell <charpellumeh@gmail.com>
2018-04-19 12:34:34 +0100

    Setup and Configure React-Router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+const ExpenseDashboard = () => (
+  <div>
+    This is from dashboard component
+  </div>
+)
+
+const AddExchangePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+)
+
+const EditExchangePage = () => (
+  <div>
+    This is from Edit expense component
+  </div>
+)
+
+const HelpPage = () => (
+  <div>
+    This is from my Help component
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+  <div>
+    <Route path="/" component={ExpenseDashboard} exact={true} />
+    <Route path="/create" component={AddExchangePage} />
+    <Route path="/edit" component={EditExchangePage} />
+    <Route path="/help" component={HelpPage} />
+
+  </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'))
