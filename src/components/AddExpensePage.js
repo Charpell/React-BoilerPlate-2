6226a869b405c4f8df704c3c73d6575c1b02108b
Charpell <charpellumeh@gmail.com>
2018-04-19 13:15:30 +0100

    Organizing our Route into different components

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const AddExchangePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+)
+
+export default AddExchangePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExchangePage = () => (
+  <div>
+    This is from Edit expense component
+  </div>
+)
+
+export default EditExchangePage;
